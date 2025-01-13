e289540d923687e15132b0c56788060b4bcfffa7
shoxxiiie <chapovskey@gmail.com>
2025-01-13 15:27:36 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class DoublyLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node* prev;
+        Node(T value) : data(value), next(nullptr), prev(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    ~DoublyLinkedList() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+
+    void AddToHead(T value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void AddToTail(T value) {
+        Node* newNode = new Node(value);
+        if (tail == nullptr) {
+            head = tail = newNode;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void DeleteFromHead() {
+        if (head == nullptr) return;
+
+        Node* temp = head;
+        if (head == tail) {
+            head = tail = nullptr;
+        } else {
+            head = head->next;
+            head->prev = nullptr;
+        }
+        delete temp;
+    }
+
+    void DeleteFromTail() {
+        if (tail == nullptr) return;
+
+        Node* temp = tail;
+        if (head == tail) {
+            head = tail = nullptr;
+        } else {
+            tail = tail->prev;
+            tail->next = nullptr;
+        }
+        delete temp;
+    }
+
+    void DeleteAll() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+        head = tail = nullptr;
+    }
+
+    void Show() const {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    DoublyLinkedList<int> list;
+
+    list.AddToHead(10);
+    list.AddToTail(20);
+    list.AddToTail(30);
+    list.Show(); // 10 20 30
+
+    list.DeleteFromHead();
+    list.Show(); // 20 30
+
+    list.DeleteFromTail();
+    list.Show(); // 20
+
+    list.AddToHead(5);
+    list.AddToTail(25);
+    list.Show(); // 5 20 25
+
+    list.DeleteAll();
+    list.Show(); // (empty line)
+
+    return 0;
+}
